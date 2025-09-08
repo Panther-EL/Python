469502c971ba4d866a054333acceb44db623f5c5
Panther-EL <agbekoelorm32@gmail.com>
2025-09-08 21:49:04 +0000

    Add files via upload

---
+++ PY.py
@@ -0,0 +1,9 @@
+# Madlibs game
+color = input('Enter your colour name:')
+adjective = input('Enter an adjective:')
+person = input('Enter the name of the person:')
+
+print('The sky is ' + color)
+print('Sugar is ' + adjective)
+print('I love ' + person)
+
